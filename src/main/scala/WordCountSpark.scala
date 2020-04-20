6ece8e17dd7856d7fee4d9ec59dfecdeb5379042
ha.paredesceballos <ha.paredesceballos@criteo.com>
2020-04-20 16:33:17 +0200

    Adding Spark version. Conf Local Master

---
+++ src/main/scala/WordCountSpark.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object WordCountSpark {
+  def main(args: Array[String]): Unit = {
+    val files = "shakespeare/"
+    val spark = SparkSession.builder.appName("Count and Save").master("local[2]").getOrCreate()
+    val stopWords = spark.read.textFile(getClass.getResource("stopWords.txt").getPath).collect()
+    val source = spark.read.textFile(getClass.getResource(files).getPath).cache()//text(getClass.getResource(files).getPath)//.cache()//.collect()
+    val text = source.rdd.flatMap(line => line
+      .replaceAll("\\W|\\d", " ")
+      .trim
+      .toLowerCase()
+      .split(" ")
+      .filter(!_.isEmpty)
+      .filterNot(stopWords.contains))
+//      .map(word => (word, 1)).reduceByKey(_+_).sortBy(-_._2).take(10)
+    val count = text.map(word => (word, 1)).reduceByKey(_+_).sortBy(-_._2)
+    count.saveAsTextFile("output/count.txt")
+    println(count.take(10).toList)
+  }
+}
