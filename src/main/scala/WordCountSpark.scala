a8d80d7adf319f939368c6e6482528c3e64aa3a8
ha.paredesceballos <ha.paredesceballos@criteo.com>
2020-04-20 16:53:05 +0200

    Save file on system

--- src/main/scala/WordCountSpark.scala
+++ src/main/scala/WordCountSpark.scala
@@ -16,7 +16,7 @@ object WordCountSpark {
       .filterNot(stopWords.contains))
 //      .map(word => (word, 1)).reduceByKey(_+_).sortBy(-_._2).take(10)
     val count = text.map(word => (word, 1)).reduceByKey(_+_).sortBy(-_._2)
-    count.saveAsTextFile("output/count.txt")
+    count.coalesce(1, false).saveAsTextFile("output/count.txt")
     println(count.take(10).toList)
   }
 }
